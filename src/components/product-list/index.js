56d27074daafea50bbb4d42628f138e0aa6c9505
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-19 12:59:20 +0530

    added product increase and decrease functionality

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -1,61 +1,98 @@
-import React, {Component} from "react";
-import "./index.css";
+/** @format */
 
-export default class ProductList extends Component {
-    constructor() {
-        super();
-    }
+import React, { useState } from 'react';
+import './index.css';
 
-    render() {
-        return (
-            <div className="layout-row wrap justify-content-center flex-70 app-product-list">
-                {this.props.products.map((product, i) => {
-                    return (
-                        <section className="w-30"
-                                 data-testid={'product-item-' + i}
-                                 key={product.id}>
-                            <div className="card ma-16">
-                                <img alt="Your Cart" src={product.image}
-                                     className="d-inline-block align-top product-image"/>
-                                <div className="card-text pa-4">
-                                    <h5 className="ma-0 text-center">{product.name}</h5>
-                                    <p className="ma-0 mt-8 text-center">${product.price}</p>
-                                </div>
-                                <div className="card-actions justify-content-center pa-4">
+const ProductList = ({ products, addToCart }) => {
+	const [quantity, setQuantity] = useState({});
+	return (
+		<div className='layout-row wrap justify-content-center flex-70 app-product-list'>
+			{products.map((product, i) => {
+				return (
+					<section
+						className='w-30'
+						data-testid={'product-item-' + i}
+						key={product.id}>
+						<div className='card ma-16'>
+							<img
+								alt='Your Cart'
+								src={product.image}
+								className='d-inline-block align-top product-image'
+							/>
+							<div className='card-text pa-4'>
+								<h5 className='ma-0 text-center'>{product.name}</h5>
+								<p className='ma-0 mt-8 text-center'>${product.price}</p>
+							</div>
+							<div className='card-actions justify-content-center pa-4'>
+								<button
+									className='x-small outlined'
+									data-testid='btn-item-add'
+									onClick={() => {
+										addToCart(
+											{
+												...product,
+												['cartQuantity']: quantity[product.id],
+											},
+											product.id
+										);
+									}}>
+									Add To Cart
+								</button>
 
-                                    <button className="x-small outlined" data-testid="btn-item-add">
-                                        Add To Cart
-                                    </button>
+								<div className='layout-row justify-content-between align-items-center'>
+									<button
+										className='x-small icon-only outlined'
+										data-testid='btn-quantity-subtract'
+										onClick={(e) => {
+											if (quantity[product.id]) {
+												setQuantity({
+													...quantity,
+													[product.id]:
+														quantity[product.id] >= 2
+															? quantity[product.id] - 1
+															: quantity[product.id],
+												});
+											}
+										}}>
+										<i className='material-icons'>remove</i>
+									</button>
 
-                                    <div className="layout-row justify-content-between align-items-center">
-                                        <button className="x-small icon-only outlined"
-                                                data-testid="btn-quantity-subtract">
-                                            <i className="material-icons">remove</i>
-                                        </button>
+									<input
+										type='number'
+										className='cart-quantity'
+										data-testid='cart-quantity'
+										value={quantity[product.id] ? quantity[product.id] : 0}
+										onChange={(e) => {
+											const { value } = e.target;
+											setQuantity({ ...quantity, [product.id]: value });
+										}}
+									/>
 
-                                        <input type="number"
-                                               disabled
-                                               className="cart-quantity" data-testid="cart-quantity"/>
-
-                                        <button className="x-small icon-only outlined"
-                                                data-testid="btn-quantity-add">
-                                            <i className="material-icons">add</i>
-                                        </button>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </section>
-                    )
-                })}
-
-            </div>
-
-        );
-    }
-}
-
-export const UpdateMode = {
-    ADD: 1,
-    SUBTRACT: 0
-}
+									<button
+										className='x-small icon-only outlined'
+										data-testid='btn-quantity-add'
+										onClick={(e) => {
+											if (quantity[product.id]) {
+												setQuantity({
+													...quantity,
+													[product.id]: quantity[product.id] + 1,
+												});
+											} else {
+												setQuantity({
+													...quantity,
+													[product.id]: 1,
+												});
+											}
+										}}>
+										<i className='material-icons'>add</i>
+									</button>
+								</div>
+							</div>
+						</div>
+					</section>
+				);
+			})}
+		</div>
+	);
+};
+export default ProductList;
