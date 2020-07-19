1b456311b3b0a7c799ea03018419c31241ca4ebe
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-19 13:01:03 +0530

    refactor page CBC to functional component and using hooks

--- src/App.js
+++ src/App.js
@@ -1,80 +1,74 @@
-import React, {Component} from 'react';
+/** @format */
+
+import React, { useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
-import ProductList from "./components/product-list";
-import Cart from "./components/cart";
+import ProductList from './components/product-list';
+import Cart from './components/cart';
 
-const title = "HackerShop";
-
-class App extends Component {
-    constructor() {
-        super();
-        const products = [...PRODUCTS].map((product, index) => {
-            product.id = index + 1;
-            product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
-            product.cartQuantity = 0;
-            return product;
-        });
-        this.state = {
-            cart: {
-                items: []
-            },
-            products
-        };
-        this.addToCart = this.addToCart.bind(this);
-        this.updateQuantity = this.updateQuantity.bind(this);
-    }
-    
-    addToCart(product, i) {
-        
-    }
-
-    updateQuantity(product, i) {
-        
-    }
-    
-    render() {
-        return (
-            <div>
-                <nav className="app-header layout-row align-items-center justify-content-center">
-                    <div className="layout-row align-items-center">
-                        <img alt="" src={logo} className="logo"/>
-                        <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
-                    </div>
-                </nav>
-                <div className="layout-row shop-component">
-                    <ProductList products={this.state.products} addToCart={this.addToCart} updateQuantity={this.updateQuantity}/>
-                    <Cart cart={this.state.cart}/>
-                </div>
-            </div>
-        );
-    }
-}
-
+const title = 'HackerShop';
 export const PRODUCTS = [
-    {
-        name: "Cap",
-        price: 5
-    },
-    {
-        name: "HandBag",
-        price: 30
-    },
-    {
-        name: "Shirt",
-        price: 35
-    },
-    {
-        name: "Shoe",
-        price: 50
-    },
-    {
-        name: "Pant",
-        price: 35
-    },
-    {
-        name: "Slipper",
-        price: 25
-    }
+	{
+		name: 'Cap',
+		price: 5,
+	},
+	{
+		name: 'HandBag',
+		price: 30,
+	},
+	{
+		name: 'Shirt',
+		price: 35,
+	},
+	{
+		name: 'Shoe',
+		price: 50,
+	},
+	{
+		name: 'Pant',
+		price: 35,
+	},
+	{
+		name: 'Slipper',
+		price: 25,
+	},
 ];
+
+const App = () => {
+	// constructor() {
+	//     super();
+	const products = [...PRODUCTS].map((product, index) => {
+		product.id = index + 1;
+		product.image = `/images/items/${product.name.toLocaleLowerCase()}.png`;
+		product.cartQuantity = 0;
+		return product;
+	});
+	const [cartItems, setCartItems] = useState([]);
+	const addToCart = (product, i) => {
+		setCartItems({ ...cartItems, [i]: product });
+	};
+
+	const updateQuantity = (product, i) => {};
+
+	return (
+		<div>
+			<nav className='app-header layout-row align-items-center justify-content-center'>
+				<div className='layout-row align-items-center'>
+					<img alt='' src={logo} className='logo' />
+					<h4 id='app-title' data-testid='app-title' className='app-title'>
+						{title}
+					</h4>
+				</div>
+			</nav>
+			<div className='layout-row shop-component'>
+				<ProductList
+					products={products}
+					addToCart={addToCart}
+					updateQuantity={updateQuantity}
+				/>
+				<Cart cart={cartItems} />
+			</div>
+		</div>
+	);
+};
 export default App;
